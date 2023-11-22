910981d847f904f7cc2730b5d8933c2ff2519889
Junior Stranner <Ju-stranner@outlook.com>
2023-11-22 11:29:48 -0300

    almost done

--- src/main/java/com/jujubaprojects/empresatech/Service/EmpresaTechService.java
+++ src/main/java/com/jujubaprojects/empresatech/Service/EmpresaTechService.java
@@ -1,6 +1,8 @@
 package com.jujubaprojects.empresatech.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
@@ -9,8 +11,10 @@ import com.jujubaprojects.empresatech.Repository.CargoDevRepository;
 import com.jujubaprojects.empresatech.Repository.DesenvolvedorRepository;
 import com.jujubaprojects.empresatech.Repository.EmpresaTechRepository;
 import com.jujubaprojects.empresatech.Repository.TipoContratoRepository;
+import com.jujubaprojects.empresatech.model.CargoDev;
 import com.jujubaprojects.empresatech.model.Desenvolvedor;
 import com.jujubaprojects.empresatech.model.EmpresaTech;
+import com.jujubaprojects.empresatech.model.TipoContrato;
 
 @Service
 public class EmpresaTechService {
@@ -42,8 +46,9 @@ public class EmpresaTechService {
             +"Empresa Tech"
             +"\n======================= "
             +"\n 0 - Voltar"
-            +"\n 1 - Embreagar Dev"
-            +"\n 2 - visualizar Devs");
+            +"\n 1 - Cadastrar Empresa"
+            +"\n 2 - visualizar Devs"
+            +"\n 3 - Embregar Dev");
             int op = Integer.parseInt(in.nextLine());
       switch(op){
         
@@ -61,15 +66,108 @@ public class EmpresaTechService {
 
     private void contratandoDev(Scanner in) {
 
+        List<Desenvolvedor> devNovos = new ArrayList<>();
+        List<CargoDev> cargoDevs = new ArrayList<>();
+        List<TipoContrato> tipoContratos = new ArrayList<>();
+
+
         System.out.println("========================="
         +"\n CONTRATAÇÂO dos DEVS"
         +"\n =========================");
+
+         System.out.println("id da Empresa-Tech ");
+        int empesaId =  Integer.parseInt(in.nextLine());
 
         System.out.println("id do Dev ");
         int devId =  Integer.parseInt(in.nextLine());
         
         System.out.println("Id do Cargo");
         int cargoDevId = Integer.parseInt(in.nextLine());
+
+         System.out.println("Id do tipo Contrato");
+        int tipoContratoIId = Integer.parseInt(in.nextLine());
+
+        
+        Optional<Desenvolvedor> devOptional = this.desenvolvedorRepository.findById(devId);
+
+        if (devOptional.isPresent()) {
+            Desenvolvedor desenvolvedor = devOptional.get();
+        //    devNovos.add(devOptional.get());
+        
+            Optional<CargoDev> cargoDevOptional = this.cargoDevRepository.findById(cargoDevId);
+        
+            if (cargoDevOptional.isPresent()) {
+                CargoDev cargoDev = cargoDevOptional.get(); 
+              //  cargoDevs.add(cargoDevOptional.get());
+        
+                Optional<TipoContrato> tipoContratoOptional = this.tipoContratoRepository.findById(tipoContratoIId);
+        
+                if (tipoContratoOptional.isPresent()) {
+                    TipoContrato tipoContrato = tipoContratoOptional.get();
+              //      tipoContratos.add(tipoContratoOptional.get());
+
+                    Optional<EmpresaTech> empresaTechOptional = this.empresaTechRepository.findById(empesaId);
+
+                  if(empresaTechOptional.isPresent()){
+                   EmpresaTech empresaTech = empresaTechOptional.get();
+
+                       empresaTech.setDesenvolvedor(desenvolvedor);
+                       empresaTech.setCargoDev(cargoDev);
+                       empresaTech.setTipoContrato(tipoContrato);
+                       this.empresaTechRepository.save(empresaTech);
+                }else{
+                      System.out.println("Empresa Tech  inexistente !");
+                  }
+                } else {
+                    System.out.println("Tipo de Contrato inexistente !");
+                }
+            } else {
+                System.out.println("Cargo inexistente !");
+            }
+        } else {
+            System.out.println("Desenvolvedor inexistente !");
+        }
+
+   /*  Optional<Desenvolvedor> devOptional = this.desenvolvedorRepository.findById(devId);
+
+if (!devOptional.isPresent()) {
+    System.out.println("Desenvolvedor inexistente !");
+    return; // ou lançar uma exceção, dependendo do seu fluxo de controle
+}
+
+Desenvolvedor desenvolvedor = devOptional.get();
+
+Optional<CargoDev> cargoDevOptional = this.cargoDevRepository.findById(cargoDevId);
+
+if (!cargoDevOptional.isPresent()) {
+    System.out.println("Cargo inexistente !");
+    return;
+}
+
+CargoDev cargoDev = cargoDevOptional.get();
+
+Optional<TipoContrato> tipoContratoOptional = this.tipoContratoRepository.findById(tipoContratoIId);
+
+if (!tipoContratoOptional.isPresent()) {
+    System.out.println("Tipo de Contrato inexistente !");
+    return;
+}
+
+TipoContrato tipoContrato = tipoContratoOptional.get();
+
+Optional<EmpresaTech> empresaTechOptional = this.empresaTechRepository.findById(empesaId);
+
+if (!empresaTechOptional.isPresent()) {
+    System.out.println("Empresa Tech inexistente !");
+    return;
+}
+
+EmpresaTech empresaTech = empresaTechOptional.get();
+empresaTech.setDesenvolvedor(desenvolvedor);
+empresaTech.setCargoDev(cargoDev);
+empresaTech.setTipoContrato(tipoContrato);
+this.empresaTechRepository.save(empresaTech);*/
+
 
     }
 
@@ -89,6 +187,7 @@ public class EmpresaTechService {
        EmpresaTech empresaTech = new EmpresaTech();
        empresaTech.setNomeEmpresa(nomeEmpresa);
        System.out.println("Empresa : "+nomeEmpresa+" Salvo no Banco !");
+       
        this.empresaTechRepository.save(empresaTech);
 
     }
